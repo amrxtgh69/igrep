824d65ca772bd191381c06bc30d3496d43cb74ef
amrxtgh <ghimireamrit619@gmail.com>
2026-03-29 23:13:23 +0545

    added: library for search

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::fs;
 use std::process;
 use std::error::Error;
 
+use igrep::search;
+
 fn main() {
     let args : Vec<String> = env::args().collect();
 
@@ -22,8 +24,10 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
 
+    for lines in search(&config.query, &contents) {
+        println!("{lines}");
+    }
     Ok(())
 }
 
