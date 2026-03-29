e3efb5709256ab36f43b9f7b43dc73c5df701f5f
amrxtgh <ghimireamrit619@gmail.com>
2026-03-29 23:05:00 +0545

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,17 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = read(config) {
+    if let Err(e) = run(config) {
         println!("Application error {e}");
         process::exit(1);
     };
 }
 
-fn read(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
     Ok(())
 }
 
