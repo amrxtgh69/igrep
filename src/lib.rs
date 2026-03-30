64d42fec6fba1c1a5d6d5a9ad652c5e78cacb89d
amrxtgh <ghimireamrit619@gmail.com>
2026-03-30 18:55:32 +0545

    add(test): lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+    vec![]
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+    }
 }
